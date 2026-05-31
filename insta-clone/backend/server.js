b629f91ac6f756baddd69f13123c789cec77d45c
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-05-31 18:04:43 +0530

    feat: set up backend with Express server, MongoDB connection, and initial routes

---
+++ insta-clone/backend/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const app = require('./src/app');
+const connectToDb = require('./src/config/database');
+
+
+
+const PORT = process.env.PORT || 3000
+
+connectToDb()
+
+
+app.listen(PORT,() => {
+    console.log(`app is listen on port ${PORT}`);
+})
+
