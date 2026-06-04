95f69e3788c68405fd26b472dcb34760e133ecf6
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-06-04 18:12:15 +0530

    feat: implement login form with styling and validation

--- insta-clone/frontend/src/features/auth/pages/Login.jsx
+++ insta-clone/frontend/src/features/auth/pages/Login.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import '../style/form.css'
+import { Link } from 'react-router'
 
 const Login = () => {
   return (
     <div className='form-container'>
         <h1>Log into Instagram</h1>
+        <form >
+            <input type="text" id='username'  placeholder='username or email' />
+            <input type="password" id='password' placeholder='Password' />
+            <button className='button primary-button'>Log in</button>
+            <p>Don't have an account ?  <Link to='/register'>Create new account</Link> </p>
+        </form>
     </div>
   )
 }
