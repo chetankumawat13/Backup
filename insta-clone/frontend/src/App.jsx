c74dddf0c58c3da22227d33a52faae55a9c1f45b
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-06-05 08:02:43 +0530

    feat: integrate authentication context and update button styles

--- insta-clone/frontend/src/App.jsx
+++ insta-clone/frontend/src/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { RouterProvider } from 'react-router'
 import { MainRoute } from './router/MainRouter'
 import './features/shared/global.css'
+import { AuthProvider } from './features/auth/auth.context'
 
 const App = () => {
   return (
-    <RouterProvider  router={MainRoute} />
+    <AuthProvider>
+      <RouterProvider  router={MainRoute} />
+    </AuthProvider>
   )
 }
 
