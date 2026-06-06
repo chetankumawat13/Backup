3ff784503424454760dd566077bea800a886e237
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-06-06 22:06:19 +0530

    feat: add user posts display and enhance profile styling

--- insta-clone/frontend/src/features/posts/components/Profile.jsx
+++ insta-clone/frontend/src/features/posts/components/Profile.jsx
@@ -42,7 +42,15 @@ const Profile = () => {
         </div>
 
         <div className="user-posts">
-
+            <div className="user-post">
+                <img src="https://ik.imagekit.io/ad6av31ld/instagram-clone/Photo_on_12-05-26_at_7.35_AM__2_Hu51YVYrP.jpg?updatedAt=1780750361088" alt="" />
+            </div>
+            <div className="user-post">
+                <img src="https://ik.imagekit.io/ad6av31ld/instagram-clone/Gemini_Generated_Image_amqufuamqufuamqu.png?updatedAt=1780731315464" alt="" />
+            </div>
+            <div className="user-post">
+                <img src="https://ik.imagekit.io/ad6av31ld/instagram-clone/Photo_on_12-05-26_at_7.35_AM__2_Hu51YVYrP.jpg?updatedAt=1780750361088" alt="" />
+            </div>
         </div>
     </div>
   )
