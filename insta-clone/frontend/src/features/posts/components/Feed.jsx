3b8beaca8dfe68243fc9892fb29b983df89b8a46
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-06-06 18:26:51 +0530

    feat: add PostCard component to display individual posts with user interactions

--- insta-clone/frontend/src/features/posts/components/Feed.jsx
+++ insta-clone/frontend/src/features/posts/components/Feed.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import '../style/feed.css'
 import { usePost } from '../hooks/usePost'
+import PostCard from './PostCard';
 
 const Feed = () => {
 
@@ -11,49 +12,17 @@ const Feed = () => {
      }, []);
 
      const posts = data?.post || [];
-     console.log(posts);
+
+     if(loading){
+        return (
+            <h1>loading.....</h1>
+        )
+     }
 
   return (
     <div className='feed-container'>
         <div className="posts">
-            <div className="post">
-                <div className="user">
-                    <div className="left-side">
-                        <img src="https://ik.imagekit.io/ad6av31ld/blank-profile-picture-973460_640.webp?updatedAt=1770787184788" alt="" />
-                        <p>chetan</p>
-                    </div>
-                    <div className="right-side">
-                        <button className='follow-btn'>Follow</button>
-                        <i className="ri-more-fill"></i>
-                    </div>
-                </div>
-                <div className="post-content">
-                    <img src="https://ik.imagekit.io/ad6av31ld/655697595_17935727496192447_7500426861642071566_n.jpg?updatedAt=1776943909131" alt="" />
-                </div>
-                <div className="post-details">
-                    <div className="left-details">
-                        <div className="like">
-                            <i className="ri-heart-3-line"></i>
-                            <p className='like-count'>99</p>
-                        </div>
-                        <div className="comment">
-                            <i className="ri-chat-3-line"></i>
-                            <p className='comment-count'>13</p>
-                        </div>
-                        <div className="send">
-                             <i className="ri-send-ins-line"></i>
-                        </div>
-                    </div>
-                    <div className="right-details">
-                        <i className="ri-bookmark-line"></i>
-                    </div>
-                </div>
-                <p className="post-caption">
-                <span className="user">chetan</span>{" "}
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit...
-                </p>
-            </div>
-            
+            <PostCard data={posts} /> 
         </div>
     </div>
   )
