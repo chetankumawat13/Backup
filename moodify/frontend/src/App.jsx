fe0174cc0c28fba867403e0916fe85f7513cc48c
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-06-09 18:39:16 +0530

    feat: set up Vite with React and create initial application structure

---
+++ moodify/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FaceExpression from './features/Expression/components/FaceExpression'
+import './features/shared/global.css'
+
+const App = () => {
+  return (
+    <div>
+      <FaceExpression />
+    </div>
+  )
+}
+
+export default App
