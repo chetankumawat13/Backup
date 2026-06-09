59e635505cd4e8a00f6295369f36b4c8a526e90e
chetankumawat13 <kumawatchetan2025@gmail.com>
2026-06-09 18:38:52 +0530

    feat: initialize backend with Express server and basic configuration

---
+++ moodify/backend/server.js
@@ -0,0 +1,10 @@
+const app = require('./src/app')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT,() => {
+    console.log(`server is listing on port ${PORT}`)
+})
